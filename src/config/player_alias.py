9233dbcf9aede86528e066f461ee338468fd3b4f
3ricKim <erickim2281@gmail.com>
2025-08-11 23:24:58 +0900

    Basic gemini integration

--- src/config/player_alias.py
+++ src/config/player_alias.py
@@ -1,4 +1,17 @@
+import google.generativeai as genai
+from dotenv import load_dotenv
+import os
 ## use llm to get player nicknames
+
+load_dotenv()
+gemini_api_key = os.getenv("gemini-api-key")
+genai.configure(api_key=gemini_api_key)
+
+model = genai.GenerativeModel('gemini-1.5-flash')
+chat = model.start_chat(history=[])
+reply = chat.send_message("Hello")
+print(reply)
+
 player_alias = [
     "Justin Jefferson",
     "justin jefferson",
