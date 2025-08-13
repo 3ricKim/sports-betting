0c1801fc01bf0f4aec2fa240917d5e2bd8cf71d4
3ricKim <erickim2281@gmail.com>
2025-08-13 20:58:15 +0900

    Ev calculation

--- src/models/baseline.py
+++ src/models/baseline.py
@@ -1,12 +1,23 @@
 #used as benchmark
-def american_to_decimal(odd):
-    if odd < 0:
-        return -odd / (-odd + 100)
+def american_to_probability(odds):
+    if odds < 0:
+        return -odds / (-odds + 100)
     else:
-        return 100 / (odd + 100)
+        return 100 / (odds + 100)
 
 def de_vig(p_over, p_under):
     return p_over / (p_over + p_under), p_under / (p_over + p_under)
 
+# american odds to payout
+def calculate_payout(odds):
+    if odds > 0:
+        return 1 + odds / 100
+    else:
+        return 1 + 100 / (-odds)
 
-print(american_to_decimal(-120))
+def calculate_ev(fair_odds, odds):
+    p = american_to_probability(fair_odds)
+    m = calculate_payout(odds)
+    return p * (m - 1) - (1 - p)
+
+print(calculate_ev(133, 129))
