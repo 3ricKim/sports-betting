874fbf17aa7751f70f5ebb8e98f04e4f06f815e8
3ricKim <erickim2281@gmail.com>
2025-08-14 00:55:24 +0900

    Adjust logistic model to use updated data

--- src/models/logistic_model.py
+++ src/models/logistic_model.py
@@ -4,11 +4,11 @@ from sklearn.metrics import accuracy_score
 import csv
 import numpy as np
 
-with open("data/odds_flat.csv") as f:
+with open("data/processed_data.csv") as f:
     reader = csv.DictReader(f)
     data = list(reader)
 
-X = np.array([[float(row["fairOdds"]), float(row["bookOdds"]), float(row["bookOverUnder"])] for row in data])
+X = np.array([[float(row["fairOdds_prob"]), float(row["bookOdds_prob"]), float(row["bookOverUnder"]), float(row["fairOverUnder"])] for row in data])
 y = np.array([int(row["label"]) for row in data])
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
